ba0ecc68b57a7964bd7dad1cb6525adf972ae8e5
bdi-carlo <benjamin.97@live.fr>
2019-02-07 11:49:11 +0100

    Second push

---
+++ app/src/main/java/com/devperso/benjamin/a2playergame/ConfigTicTacToeActivity.java
@@ -0,0 +1,13 @@
+package com.devperso.benjamin.a2playergame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfigTicTacToeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_tic_tac_toe);
+    }
+}
